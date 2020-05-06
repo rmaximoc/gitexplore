35b8df6cee53de38c8d6cb81b7e7f1d6d465a8b8
Rodrigo <rodrigomaximocano@gmail.com>
2020-05-05 21:33:43 -0300

    Add styled components

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './style';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Git</Title>;
+};
+
+export default Dashboard;
