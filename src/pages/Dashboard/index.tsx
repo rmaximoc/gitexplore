924b94732cf18685fcf7aa4ff9dbcdf741057076
Rodrigo <rodrigomaximocano@gmail.com>
2020-05-05 22:43:08 -0300

    Add repos list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './style';
+
+import logo from '../../assets/images/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Git</Title>;
+  return (
+    <>
+      <img src={logo} alt="gitexplorer" />
+      <Title>Explore Git repos</Title>
+
+      <Form action="">
+        <input type="text" placeholder="Search the repo" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img src="" alt="" />
+          <div>
+            <strong>Title</strong>
+            <p>description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
