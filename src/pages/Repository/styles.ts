2c6887b0dfa13fa308793cecbbcaa28de50a5a7e
Rodrigo <rodrigomaximocano@gmail.com>
2020-05-09 01:29:18 -0300

    Add api gets and issues list

---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+  }
+
+  &:hover {
+    color: #666;
+  }
+
+  svg {
+    margin-right: 4px;
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      & + li {
+        margin-left: 80px;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      flex: 1;
+      margin: 0 16px;
+
+      strong {
+        font-size: 20px;
+        color: #222222;
+      }
+
+      p {
+        font-size: 18px;
+        color: #95a3b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #222222;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 30px;
+`;
